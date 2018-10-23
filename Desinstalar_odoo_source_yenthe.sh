15407b3fb3fb52493dce99db87195663cb9e37b3
DavidLizasoain <44134307+DavidLizasoain@users.noreply.github.com>
2018-10-23 08:52:17 +0200

    Update Desinstalar_odoo_source_yenthe.sh

--- Desinstalar_odoo_source_yenthe.sh
+++ Desinstalar_odoo_source_yenthe.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 #################################################################################################################
 
-# script para desinstalar odoo 10 y postgresql 9.5 INSTALADO DESDE CODIGO FUENTE (yenthe)
-# Deja el sistema linux listo para volver a instalar Odoo
+# script para desinstalar odoo 10 y postgresql 9.5 INSTALADO DESDE PACKAGE (web Odoo)
+# Deja el sistema linux listo para volver a instalar Odoo (desde source, pero no desde package)
 
 # STOP SERVERS
-  sudo service odoo-server stop
+  sudo service odoo stop
   sudo service postgresql stop
 
-# REMOVE ALL ODOO FILES
-  sudo rm -R /odoo
-# REMOVE CONFIG FILES
-  sudo rm -f /etc/odoo-server.conf
-  sudo update-rc.d -f odoo-server remove 
-  sudo rm -f /etc/init.d/odoo-server
+# REMOVE ODOO PACKAGE
+  sudo apt-get remove odoo
+  
+# REMOVE PACKAGES INSTALLED WITH ODOO (POSTGRESQL ALONG OTHERS)  
+  sudo apt autoremove
+  
+# PURGE IN ORDER TO REMOVE CONFIG FILES (NECESSARY?)
+  sudo apt-get --purge remove odoo/* -y
+  sudo apt-get --purge remove postgresql\* -y
+
 # REMOVE USER AND USER GROUP
 # Si hay algún proceso ejecutado por los usuarios, cerrarlo antes con pkill
 # para ver los procesos ejecutados por un usuario $ ps -u odoo
@@ -23,17 +27,6 @@
   sudo userdel -r odoo
   sudo groupdel postgres
   sudo groupdel odoo
-
-# REMOVE DATABASE
-  sudo apt-get remove postgresql -y
-  sudo apt-get --purge remove postgresql\* -y
-#  sudo rm -rf /etc/postgresql/
-#  sudo rm -rf /etc/postgresql-common/
-#  sudo rm -rf /var/lib/postgresql/
-
-# ELIMINAR WKHTMLTOPDF Y WKHTMLTOIMAGE
-  sudo rm /usr/bin/wkhtmltopdf
-  sudo rm /usr/bin/wkhtmltoimage
-  
+ 
 # ELIMINAR DIRECTORIO LOG
-  sudo rm -R /var/log/odoo/
+#  sudo rm -R /var/log/odoo/
